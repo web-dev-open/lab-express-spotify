1a6772870fe79d4da9b116bda0eddcbc0f9d09e9
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-09-27 01:53:22 -0700

    done and styled

--- app.js
+++ app.js
@@ -56,16 +56,60 @@ app.get('/artist-search', (req, res, next) => {
 spotifyApi
 .searchArtists(name)
 	.then(data => {
-	console.log('The received data from the API: ', data.body);
-    res.render("artist-search-results.hbs", {data: response.data})
+	// console.log('The received data from the API: ', data.body.artists.items);
+	// for (let i = 0; i < data.body.artists.items.length; i++){
+	// 	console.log(data.body.artists.items[i].images)
+	// }
+    res.render("artist-search-results.hbs", {data: data.body.artists.items})
 	// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
 })
 
 .catch(err => console.log('The error while searching artists occurred: ', err));
-
-
   //make API call here
 })
+
+
+// Passing a callback - get Elvis' albums in range [20...29]
+app.get('/albums/:id', (req, res, next) => {
+	let {id} = req.params
+spotifyApi
+  .getArtistAlbums(id, { limit: 10})
+  .then(data => {
+	// for (let i = 0; i < data.body.items.length; i++){
+	// 	console.log(data.body.items[i].images)
+	// }
+	// console.log(data.body.items)
+	// console.log('The received data from the API: ', data.body.artists.items);
+
+    res.render("albums.hbs", {data: data.body.items})
+	// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+})
+
+.catch(err => console.log('The error while searching artists occurred: ', err));
+  //make API call here
+});
+
+
+app.get('/albums/:id/tracks', (req, res, next) => {
+	let {id} = req.params
+spotifyApi.getAlbumTracks(id, { limit : 5, offset : 1 })
+
+
+
+  .then(data => {
+	// for (let i = 0; i < data.body.items.length; i++){
+	// 	console.log(data.body.items[i].preview_url)
+	// }
+	// console.log(data.body.items.preview_url)
+	// console.log('The received data from the API: ', data.body.artists.items);
+
+    res.render("tracks.hbs", {data: data.body.items})
+	// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+})
+
+.catch(err => console.log('The error while searching artists occurred: ', err));
+  //make API call here
+});
 
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
